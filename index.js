58d562b0a97879febd5dab382d893ab3d11fc8e0
Maciej Goluch <maciek_kom@op.pl>
2023-07-16 21:19:02 +0200

    add scroller

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mainScroller = document.querySelector(".rootScroller");
+  const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrottled = false;
+  document.addEventListener("wheel", (e) => {
+    if (isThrottled) return;
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+    }, 1000);
+
+    const direction = e.deltaY > 0 ? 1 : -1;
+    scroll(direction);
+  });
+
+  const scroll = (direction) => {
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex = currentSectionIndex + direction;
+    scrollToSection();
+  };
+
+  const scrollToSection = () => {
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+});
